01c54554820012c565a842d08161bb39eaf003bc
yikaus <yikaus@gmail.com>
2014-07-14 05:23:31 +0000

    v0.0.2 drop

--- vucli/vucli.py
+++ vucli/vucli.py
@@ -19,7 +19,7 @@ import sys
 import os
 import argparse
 #import simplejson as json
-import readline
+import readline,glob
 
 API_BASE_URL = "https://api.vultr.com/v1/"
 REGIONS_AVAILABILITY_URL = ""
@@ -27,6 +27,8 @@ PLANS_LIST_URL = "plans/list"
 REGIONS_LIST_URL = "regions/list"
 OS_LIST_URL = "os/list"
 SNAPSHOT_LIST_URL = "snapshot/list?api_key="
+SNAPSHOT_CREATE_URL = "snapshot/create?api_key="
+SNAPSHOT_DESTROY_URL = "snapshot/destroy?api_key="
 SERVER_LIST_URL = "server/list?api_key="
 SERVER_REBOOT_URL = "server/reboot?api_key="
 SERVER_HALT_URL="server/halt?api_key="
@@ -34,11 +36,42 @@ SERVER_START_URL="server/start?api_key="
 SERVER_DESTROY_URL="server/destroy?api_key="
 SERVER_CREATE_URL="server/create?api_key="
 SERVER_REINSTALL_URL="server/reinstall?api_key="
+STARTUPSCRIPT_LIST_URL="startupscript/list?api_key="
+STARTUPSCRIPT_DESTROY_URL="startupscript/destroy?api_key="
+STARTUPSCRIPT_CREATE_URL="startupscript/create?api_key="
+
+SNAPHOST_OS_ID = 164
+CUSTOM_OS_ID = 159
+
+ops_by_subid = [SERVER_REBOOT_URL,SERVER_HALT_URL,SERVER_START_URL,SERVER_DESTROY_URL,SERVER_REINSTALL_URL]
+ops_by_snapshotid = [SNAPSHOT_DESTROY_URL]
+ops_by_scriptid = [STARTUPSCRIPT_DESTROY_URL]
 
 userkey = ""
 interactive_mode = False
-cmds=['planlist', 'regionlist','oslist','snapshotlist','serverlist','reboot','halt','start','destroy','reinstall','create','changekey','quit','help']
+cmds=['planlist',
+     'regionlist',
+	 'oslist',
+	 'snapshotlist',
+	 'snapshotdestroy',
+	 'newsnapshot',
+	 'serverlist',
+	 'reboot',
+	 'halt',
+	 'start',
+	 'destroy',
+	 'reinstall',
+	 'create',
+	 'scriptlist',
+	 'scriptdestroy',
+	 'newscript',
+	 'changekey',
+	 'quit',
+	 'help']
 
+cmdsneedkey=['serverlist','reboot','halt','start','destroy','reinstall','create',
+			'scriptlist','scriptdestroy','newscript','snapshotlist','snapshotdestroy','newsnapshot']
+	 
 '''
 def printjson(obj): 
     res = json.dumps(obj, sort_keys=True, indent=4, separators=(',', ': '))
@@ -65,10 +98,10 @@ def jsontotable(obj):
 def serverjsontotable(obj): 
 
 	table =[]
-	headerline = ["SUBID","os","ram","vcpu","disk","ip","location","DCID","VPSPLANID","Cost","status"]
+	headerline = ["SUBID","os","ram","vcpu","disk","ip","location","DCID","VPSPLANID","Cost","Charge","status"]
 	table.append(headerline)
 	for k,v in obj.iteritems():
-		line =[v["SUBID"],v["os"],v["ram"],v["vcpu_count"],v["disk"],v["main_ip"],v["location"],v["DCID"],v["VPSPLANID"],v["cost_per_month"],v["power_status"]]	
+		line =[v["SUBID"],v["os"],v["ram"],v["vcpu_count"],v["disk"],v["main_ip"],v["location"],v["DCID"],v["VPSPLANID"],v["cost_per_month"],v["pending_charges"],v["power_status"]]	
 		table.append(line)
 	return table
 
@@ -117,7 +150,18 @@ def pprint_table(out, table):
             col = format_num(row[i]).rjust(col_paddings[i] + 2)
             print >> out, col,
         print >> out		
-	
+
+def script_print(obj):
+	for k,v in obj.iteritems():
+		print "%s\t%s" % (v["name"],v["date_modified"])
+		print "==========Script Content=================="
+		print v["script"]
+		print "==========Script Content End=============="
+		print 
+	print "==========Script lists===================="
+	for k,v in obj.iteritems():
+		print "%s\t%s\t%s\t%s" % (k,v["name"],v["date_modified"],v["date_modified"])
+	print
 	
 def login():
 	global userkey
@@ -127,19 +171,77 @@ def login():
 def changekey():
 	global userkey 
 	userkey = raw_input ("please enter your new user key :")
-		
+
+	
 def rest_server_api(operation,key="",args=[]):
 	url = API_BASE_URL + operation + key
-	payload = {'SUBID': int(args[0])}
+	if operation in ops_by_subid:
+		payload = {'SUBID': int(args[0])}
+	elif operation in ops_by_scriptid:
+		payload = {'SCRIPTID': int(args[0])}
+	elif operation in ops_by_snapshotid:
+		payload = {'SNAPSHOTID': args[0]}
 	response = requests.post(url,data=payload)
 	if response.status_code == 200 :
 		print "Operation Successful!"
 	else :
 		print "Operation Failed! with HTTP %s Error!" % response.status_code
+		
+
+def rest_snapshot_create_api(operation,key="",args=[]):
+	url = API_BASE_URL + operation + key
+	payload = {'SUBID': int(args[0]),'description':args[1]}
+	response = requests.post(url,data=payload)
+	try :
+		response.json()
+		print "Snapshot Created with SNAPSHOTID  "
+		printjson(response.json())
+	except Exception :
+		#not json ,something wrong
+		print response.text
+		return	
+		
+		
+def rest_script_create_api(operation,key="",args=[]):
+	url = API_BASE_URL + operation + key
+	content = ""
+	fname = args[1]
+	if not os.path.isfile(fname) :
+		print "%s not exists" % fname
+		return
+	with open (fname, "r") as scriptfile:
+		content=scriptfile.read()
+	payload = {'name': args[0],'script':content }
+	response = requests.post(url,data=payload)
+	try :
+		response.json()
+		print "Start Script Created with SCRIPTID "
+		printjson(response.json())
+	except Exception :
+		#not json ,something wrong
+		print response.text
+		return
+		
+
 
 def rest_server_create_api(operation,key="",args=[]):
 	url = API_BASE_URL + operation + key
-	payload = {'DCID': int(args[0]),'VPSPLANID': int(args[1]),'OSID': int(args[2])}
+
+	if len(args) == 4 :
+		try :
+			payload = {'DCID': int(args[0]),'VPSPLANID': int(args[1]),'OSID': int(args[2]),'SCRIPTID':int(args[3])}
+		except Exception:
+			print "StartScript can not be used with snapshot , only with standard vultr os image!"
+			print 
+			return
+	else:
+		#3 args 
+		try :
+			payload = {'DCID': int(args[0]),'VPSPLANID': int(args[1]),'OSID': int(args[2])}
+		except Exception:
+			payload = {'DCID': int(args[0]),'VPSPLANID': int(args[1]),'SNAPSHOTID':args[2],'OSID':SNAPHOST_OS_ID}
+	
+	
 	response = requests.post(url,data=payload)
 	try :
 		response.json()
@@ -169,6 +271,8 @@ def rest_list_api(operation,key=""):
 		pprint_table(out,serverjsontotable(response.json()))
 	elif operation == OS_LIST_URL:
 		pprint_table(out,osjsontotable(response.json()))
+	elif operation == STARTUPSCRIPT_LIST_URL:
+	    script_print(response.json())
 	else:
 		pprint_table(out,jsontotable(response.json()))
 	
@@ -186,6 +290,14 @@ def snapshot_list():
 	login()
 	rest_list_api(SNAPSHOT_LIST_URL,userkey)
 
+def snapshot_create(args):
+	login()
+	rest_snapshot_create_api(SNAPSHOT_CREATE_URL,userkey,args)
+	
+def snapshot_destroy(args):
+	login()
+	rest_server_api(SNAPSHOT_DESTROY_URL,userkey,args)
+	
 def server_list():
 	login()
 	rest_list_api(SERVER_LIST_URL,userkey)
@@ -214,6 +326,18 @@ def server_create(args):
 	login()
 	rest_server_create_api(SERVER_CREATE_URL,userkey,args)	
 	
+def script_list():
+	login()
+	rest_list_api(STARTUPSCRIPT_LIST_URL,userkey)	
+
+def script_create(args):
+	login()
+	rest_script_create_api(STARTUPSCRIPT_CREATE_URL,userkey,args)
+
+def script_destroy(args):
+	login()
+	rest_server_api(STARTUPSCRIPT_DESTROY_URL,userkey,args)
+	
 def interactive():
     global interactive_mode
     interactive_mode =True
@@ -225,7 +349,7 @@ def interactive():
     print "#                        #"
     print "##########################"
     print ""
-    print "Type help to load help page ."
+    print "Type help to load help page . Or just hit Tab"
     print ""
 	
     command =''
@@ -243,12 +367,13 @@ def interactive():
 
 	
 def complete(text, state):
-    for cmd in cmds:
+	for cmd in cmds:
 		if cmd.startswith(text):
-		    if not state:
-			return cmd
-		    else:
-			state -= 1
+			if not state:
+				return cmd
+			else:
+				state -= 1
+
 			
 def runcmd(command,args):
 	
@@ -256,6 +381,8 @@ def runcmd(command,args):
 		'regionlist':regions_list,
 		'oslist':os_list,
 		'snapshotlist':snapshot_list,
+		'snapshotdestroy':snapshot_destroy,
+		'newsnapshot':snapshot_create,
 		'serverlist':server_list,
 		'reboot':server_reboot,
 		'halt':server_halt,
@@ -263,6 +390,9 @@ def runcmd(command,args):
 		'create':server_create,
 		'reinstall':server_reinstall,
 		'destroy':server_destroy,
+		'scriptlist':script_list,
+		'scriptdestroy':script_destroy,
+		'newscript':script_create,
 		'changekey':changekey,
 		'help':help,
 		'quit':sys.exit
@@ -270,12 +400,20 @@ def runcmd(command,args):
 	if command not in cmds:
 		print 'Invilad command! Press Tab key or use help command list all avaiable command.'
 		print ''
-	elif command in ['reboot','halt','start','destroy','reinstall'] and not args:
-		print 'This command need argument!'
+	elif command in ['reboot','halt','start','destroy','reinstall','scriptdestroy','snapshotdestroy'] and not args:
+		print 'This command need 1 argument!'
 		print ''
-	elif command in ['create'] and len(args)<3:
-		print 'missing arguments : create <DCID> <PLANID> <OSID>'
+	elif command in ['create'] and len(args) not in [3,4]:
+		print 'Arguments Error: create <DCID> <PLANID> <OSID>|<SNAPSHOTID> [<STARTSCRIPTID>] '
+		print '** Startscript can not be used with snapshot'
 		print ''
+	elif command in ['newscript'] and len(args)<2:
+		print 'Arguments Error : newscript <NAME> <SCRIPTPATH>'
+		print ''
+	elif command in ['newsnapshot'] and len(args)<2:
+		print 'Arguments Error : newsnapshot <SUBID> <DESCIRIPTION>'
+		print ''
+		
 	else:
 		if not args :
 			cmddict[command]()
@@ -299,6 +437,11 @@ def main():
 			parser.add_argument('-d', '--dc', dest = 'dcid',required=False)
 			parser.add_argument('-p', '--plan', dest = 'planid',required=False)
 			parser.add_argument('-os', '--os', dest = 'osid',required=False)
+			parser.add_argument('-f', '--file', dest = 'scriptfile',required=False)
+			parser.add_argument('-st', '--scriptid', dest = 'scriptid',required=False)
+			parser.add_argument('-sn', '--scriptname', dest = 'scriptname',required=False)
+			parser.add_argument('-ss', '--snapshotid', dest = 'snapshotid',required=False)
+			parser.add_argument('-sd', '--snapshotdesc', dest = 'snapshotdesc',required=False)
 
 			args = parser.parse_args()
 
@@ -307,6 +450,8 @@ def main():
 			pass
 		except  Exception ,e:
 			help()
+			print e
+			
 			'''
 			parser.print_help()
 
@@ -328,7 +473,7 @@ def main():
 		if args.userkey :
 			global userkey 
 			userkey = args.userkey
-		elif args.command in ['snapshotlist','serverlist','reboot','halt','start','destroy','reinstall','create']:
+		elif args.command in cmdsneedkey:
 			print
 			print "please provide apikey with -k option"
 			print
@@ -337,22 +482,63 @@ def main():
 		if args.command :
 		
 			
-			if args.command in ['planlist', 'regionlist','oslist','snapshotlist','serverlist']:
+			if args.command in ['planlist', 'regionlist','oslist','snapshotlist','serverlist','scriptlist']:
 				runcmd(args.command,[])
 			elif args.command in ['reboot','halt','start','destroy','reinstall']:
 				if not args.subid :
 					print
-					print "please provide subid with -id option"
+					print "Arguments Error , Please add -id <SUBID>"
 					print
 					sys.exit()
 				runcmd(args.command,[args.subid])
-			elif args.command in ['create']:
-				if not args.dcid and not args.osid and not args.planid : 
+			elif args.command in ['newsnapshot']:
+				if not args.subid or not args.snapshotdesc:
 					print
-					print "To create server , please also provide dcid with -d option , planid with -p option and osid with -os option"
+					print "Arguments Error ,Use vucli newsnapshot -k <USERKEY> -id <SUBID> -sd <DESCIRIPTION>"
 					print
 					sys.exit()
-				runcmd(args.command,[args.dcid,args.planid,args.osid])
+				runcmd(args.command,[args.subid,args.snapshotdesc])
+			elif args.command in ['snapshotdestroy']:
+				if not args.snapshotid :
+					print
+					print "Arguments Error ,Use vucli snapshotdestroy -k <USERKEY> -ss <SNAPSHOTID>"
+					print
+					sys.exit()
+				runcmd(args.command,[args.snapshotid])
+			elif args.command in ['newscript']:
+				if not args.scriptname or not args.scriptfile:
+					print
+					print "Arguments Error ,Use vucli newscript -k <USERKEY> -sn <SCRIPTNAME> -f <SCRIPTPATH>"
+					print
+					sys.exit()
+				runcmd(args.command,[args.scriptname,args.scriptfile])
+			elif args.command in ['scriptdestroy']:
+				if not args.scriptid :
+					print
+					print "Arguments Error ,Use vucli scriptdestroy -k <USERKEY> -st <SCRIPTID>"
+					print
+					sys.exit()
+				runcmd(args.command,[args.scriptid])
+			elif args.command in ['create']:
+				if not args.dcid or not args.planid or not (args.osid or args.snapshotid) or (args.osid and args.snapshotid): 
+					print
+					print "Arguments Error ,Use vucli create -d <DCID> -p <PLANID> -os <OSID>|-ss <SNAPSHOTID> [-st <SCRIPTID>]"
+					print
+					print "Can only choose <OSID> or <SNAPSHOTID> at one time"
+					print '** Startscript can not be used with snapshot'
+					print
+					sys.exit()
+				createarg = []	
+				if args.osid :
+					createarg=[args.dcid,args.planid,args.osid]
+				elif args.snapshotid:
+					createarg=[args.dcid,args.planid,args.snapshotid]
+				
+				if args.scriptid :
+					createarg.append(args.scriptid)
+				
+				
+				runcmd(args.command,createarg)
 		else:
 			interactive()
 	except (KeyboardInterrupt,EOFError) as e:
@@ -362,7 +548,6 @@ def main():
 
 
 def help():
-#'planlist', 'regionlist','oslist','snapshotlist','serverlist','reboot','halt','start','destroy','reinstall','create','changekey','quit'
 	print
 	print "Interactive Usage : vucli"
 	print
@@ -373,6 +558,8 @@ def help():
 	print "     regionlist        list all available regions"
 	print "     oslist            list all operating system images"
 	print "     snapshotlist      list all snapshots"
+	print "     newsnapshot       create a snapshot with subid"
+	print "     snapshotdestroy   delete a snapshot"
 	print "     serverlist        list all servers"
 	print "     reboot            restart server by server id"
 	print "     halt              hard stop server by server id"
@@ -380,6 +567,10 @@ def help():
 	print "     destroy           destroy server by server id"
 	print "     reinstall         reinstall server by server id"
 	print "     create            create new server "
+	print "     scriptlist        list all start script"
+	print "     scriptdestroy     delete particular start script"
+	print "     newscript         create new server "
+	print "     create            create start script "
 	print "     changekey         reset API key (only worked in interactive mode)"
 	print "     quit              quit interactive mode (only worked in interactive mode)"
 	
@@ -390,6 +581,20 @@ def help():
 		print "     -d  <DCID>        provide datacenter DCID"
 		print "     -p  <PLANID>      provide PLANID"
 		print "     -os <OSID>        provide OS image ID"
+		print "     -f <SCRIPTPATH>   provide script file path"
+		print "     -st <SCRIPTID>    provide script id"
+		print "     -sn <SCRIPTNAME>  provide script name"
+		print "     -ss <SUBID>       provide server id to create a snapshot"
+		print "     -sd <SNAPDESC>    provide snapshot description"
+		print
+		
+		print "Examples:"
+		
+		print "  create instance with OSID 128 at DCID 5 and planid 29 ,script id 245"
+		print "      vucli create -k <yourkey> -d 5 -p 29 -os 128 -st 245"
+		print
+		print "  create instance with SNAPSHOTID 53bdd6e0d6414 at DCID 5 and planid 29"
+		print "      vucli create -k <yourkey> -d 5 -p 29 -ss 53bdd6e0d6414"
 		print
 	
 
